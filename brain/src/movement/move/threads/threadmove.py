af8838269e02d10b82afe0df6e8eb520e76270e6
Deblisus <rafaelrotar8@gmail.com>
2025-01-20 17:38:20 +0200

    stop

--- brain/src/movement/move/threads/threadmove.py
+++ brain/src/movement/move/threads/threadmove.py
@@ -1,6 +1,10 @@
+import base64
 import time
+
+import cv2
+import numpy as np
 from src.templates.threadwithstop import ThreadWithStop
-from src.utils.messages.allMessages import (DrivingMode, SpeedMotor, mainCamera)
+from src.utils.messages.allMessages import (DrivingMode, SpeedMotor, mainCamera, serialCamera)
 from src.utils.messages.messageHandlerSubscriber import messageHandlerSubscriber
 from src.utils.messages.messageHandlerSender import messageHandlerSender
 class threadmove(ThreadWithStop):
@@ -20,14 +24,43 @@ class threadmove(ThreadWithStop):
 
         self.speed = messageHandlerSender(self.queuesList, SpeedMotor)
         self.driving_mode = messageHandlerSubscriber(self.queuesList, DrivingMode, "lastOnly", True)
+        self.camera = messageHandlerSubscriber(self.queuesList, serialCamera, "lastOnly", True)
+
+        self.driveState = True
 
     def run(self):
         while self._running:
             drv = self.driving_mode.receive()
+            cam = self.camera.receive()
             if drv is not None:
                 if drv == "auto":
                     self.speed.send("50")
                     print("a")
+            if cam is not None:
+                image_data = base64.b64decode(cam)
+                img = np.frombuffer(image_data, dtype=np.uint8)
+                image = cv2.imdecode(img, cv2.IMREAD_COLOR)
+                nrPix = self.countRedPixels(image)
+                height, width, channels = image.shape
+                MP = height*width
+                if nrPix > MP/3 and self.driveState:
+                    self.speed.send("0")
+                    self.driveState = False
+                elif not nrPix > MP/3 and self.driveState == False:
+                    self.speed.send("50")
+                    self.driveState = True
+                    
+
+
+    def countRedPixels(image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_red = np.array([160,140,50]) 
+        upper_red = np.array([180,255,255])
+
+        imgThreshHigh = cv2.inRange(hsv, lower_red, upper_red)
+        nr_pix = np.sum(imgThreshHigh == 255)
+        return nr_pix
+
     def subscribe(self):
         """Subscribes to the messages you are interested in"""
         pass
