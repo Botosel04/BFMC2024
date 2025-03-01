7b750b7549744cbf2e7d87eea6bf7d111fc7c35a
Deblisus <rafaelrotar8@gmail.com>
2025-03-01 19:32:14 +0200

    Update threadlaneDetection.py

--- brain/src/perception/laneDetection/threads/threadlaneDetection.py
+++ brain/src/perception/laneDetection/threads/threadlaneDetection.py
@@ -39,7 +39,7 @@ class threadlaneDetection(ThreadWithStop):
                 image = cv2.imdecode(img, cv2.IMREAD_COLOR)
 
                 steer_angle, processed_image = getSteer(image)
-                steer_angle = -steer_angle
+                steer_angle = -steer_angle * 10
                 _, processed_image_jpg = cv2.imencode(".jpg", processed_image)
                 processed_image_bytes = base64.b64encode(processed_image_jpg).decode("utf-8")
                 self.processedCamera.send(processed_image_bytes)
