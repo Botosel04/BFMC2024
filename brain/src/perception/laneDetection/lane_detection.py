5639230d01bf1d32d8d5c6742e9099998f93e19e
Deblisus <rafaelrotar8@gmail.com>
2025-03-01 19:25:19 +0200

    Update lane_detection.py

--- brain/src/perception/laneDetection/lane_detection.py
+++ brain/src/perception/laneDetection/lane_detection.py
@@ -264,7 +264,7 @@ prev_right_fit2 = []
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 150, 300)
+    canny = cv2.Canny(blur, 150, 200)
     return canny
 
 class Lane:
