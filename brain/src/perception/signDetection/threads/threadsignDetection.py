1211c9e5c9ae4859c867b469f890975ed05a7ebf
Deblisus <rafaelrotar8@gmail.com>
2025-03-15 20:40:29 +0200

    Update threadsignDetection.py

--- brain/src/perception/signDetection/threads/threadsignDetection.py
+++ brain/src/perception/signDetection/threads/threadsignDetection.py
@@ -2,9 +2,9 @@ from src.templates.threadwithstop import ThreadWithStop
 from src.utils.messages.allMessages import (mainCamera, serialCamera)
 from src.utils.messages.messageHandlerSubscriber import messageHandlerSubscriber
 from src.utils.messages.messageHandlerSender import messageHandlerSender
+from src.perception.ultralytics import YOLO
 import base64
 import numpy as np
-from ultralytics import YOLO
 class threadsignDetection(ThreadWithStop):
     """This thread handles signDetection.
     Args:
