f8c917d8d6b78536435f35f1609e8b9b1c9ac60c
Deblisus <rafaelrotar8@gmail.com>
2025-03-17 17:33:27 +0200

    fix

--- brain/src/perception/signDetection/threads/threadsignDetection.py
+++ brain/src/perception/signDetection/threads/threadsignDetection.py
@@ -27,7 +27,7 @@ class threadsignDetection(ThreadWithStop):
         self.frameCount = 0
         self.model = YOLO("src/perception/models/best_ncnn_model")
         #self.model = YOLO("src/perception/models/best.pt")
-        self.confList = [0.0, 0.3, 0.7, 0.8, 0.0, 0.9, 0.8, 0.9]
+        self.confList = [0.0, 0.3, 0.7, 0.8, 0.0, 0.5, 0.9, 0.8, 0.9]
 
         self.events = [messageHandlerSender(self.queuesList, CrosswalkSign), messageHandlerSender(self.queuesList, HighwayEntrySign), messageHandlerSender(self.queuesList, HighwayExitSign), messageHandlerSender(self.queuesList, NoEntryRoadSign), messageHandlerSender(self.queuesList, OneWayRoadSign), messageHandlerSender(self.queuesList, ParkingSign), messageHandlerSender(self.queuesList, PrioritySign), messageHandlerSender(self.queuesList, RoundaboutSign), messageHandlerSender(self.queuesList, StopSign)]
 
